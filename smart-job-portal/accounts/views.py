b081cf8de776131180199222cd8a4e67fe229dae
Rohan Giri <rohangiri005@gmail.com>
2026-04-22 18:40:11 -0400

    sign up  of account is created

--- smart-job-portal/accounts/views.py
+++ smart-job-portal/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm,Profileforms
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegisterForm(request.POST)
+        profile_form = Profileforms(request.POST, request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            messages.success(request, "Account created successfully!")
+            return redirect('login')
+    else:
+        user_form = UserRegisterForm()
+        profile_form = Profileforms()
+
+    return render(request, 'accounts/register.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
 
 # Create your views here.
